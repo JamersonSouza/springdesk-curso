190e49a2064ab32a7e1066566dcd62f2d113346c
Jamerson Souza <jamersonsouza@outlook.com>
2022-10-29 11:49:11 -0300

    Controller do tecnico com método para salvar

--- src/main/java/tech/jamersondev/springdesk/controllers/tecnico/TecnicoController.java
+++ src/main/java/tech/jamersondev/springdesk/controllers/tecnico/TecnicoController.java
@@ -1,16 +1,27 @@
 package tech.jamersondev.springdesk.controllers.tecnico;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import tech.jamersondev.springdesk.Enums.Perfil;
 import tech.jamersondev.springdesk.model.Tecnico;
+import tech.jamersondev.springdesk.repository.TecnicoRepository;
+import tech.jamersondev.springdesk.util.UploadUtil;
 
 @Controller
 @RequestMapping("/tecnico")
 public class TecnicoController {
+    
+    @Autowired
+    private TecnicoRepository tecRepository;
+
 
     @GetMapping("/cadastro")
     public ModelAndView cadastro(Tecnico tecnico){
@@ -19,5 +30,32 @@ public class TecnicoController {
         mv.addObject("perfils", Perfil.values());
         return mv;
     }
+
+    @PostMapping("/cadastro-tecnico")
+    public ModelAndView cadastro(@ModelAttribute Tecnico tecnico, @RequestParam("file") MultipartFile imagem){
+       ModelAndView mv =  new ModelAndView("tecnico/cadastro");
+       mv.addObject("tecnico", tecnico);
+
+       try {
+        if(UploadUtil.fazerUploadImagem(imagem)){
+            tecnico.setImagem(imagem.getOriginalFilename());
+        }
+        tecRepository.save(tecnico);
+        System.out.println("Salvo com sucesso: " + tecnico.getNome() + " " + tecnico.getImagem());
+        return home();
+       } catch (Exception e) {
+            mv.addObject("msgErro", e.getMessage());
+            System.out.println("Erro ao salvar " + e.getMessage());
+            return mv;
+       }
+
+    }
+
+    @GetMapping("/home-tecnico")
+    public ModelAndView home(){
+        ModelAndView mv =  new ModelAndView("home/index");
+        return mv;
+    }
+
     
 }
