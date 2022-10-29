02ba70a0989148572c99f33d2bd96662db0dd52c
Jamerson Souza <jamersonsouza@outlook.com>
2022-10-29 11:46:22 -0300

    Controller com métodos para salvar um cliente

--- src/main/java/tech/jamersondev/springdesk/controllers/cliente/ClienteController.java
+++ src/main/java/tech/jamersondev/springdesk/controllers/cliente/ClienteController.java
@@ -1,22 +1,58 @@
 package tech.jamersondev.springdesk.controllers.cliente;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import tech.jamersondev.springdesk.Enums.Perfil;
 import tech.jamersondev.springdesk.model.Cliente;
+import tech.jamersondev.springdesk.repository.ClienteRepository;
+import tech.jamersondev.springdesk.util.UploadUtil;
 
 @Controller
-@RequestMapping("/cliente")
+@RequestMapping("cliente")
 public class ClienteController {
 
-    @GetMapping("/cadastro")
+    @Autowired
+    private ClienteRepository clienteRepo;
+
+    @GetMapping("cadastro")
     public ModelAndView cadastro(Cliente cliente){
         ModelAndView mv = new ModelAndView("cliente/cadastro");
-        mv.addObject("usuario", cliente);
+        mv.addObject("usuario", new Cliente());
         mv.addObject("perfils", Perfil.values());
+        return mv;
+    }
+
+    @PostMapping("/cadastro-cliente")
+    public ModelAndView cadastro(@ModelAttribute Cliente cliente, @RequestParam("file") MultipartFile imagem){
+       ModelAndView mv =  new ModelAndView("cliente/cadastro");
+       mv.addObject("usuario", cliente);
+
+       try {
+        if(UploadUtil.fazerUploadImagem(imagem)){
+            cliente.setImagem(imagem.getOriginalFilename());
+        }
+        clienteRepo.save(cliente);
+        System.out.println("Salvo com sucesso: " + cliente.getNome() + " " + cliente.getImagem());
+        return home();
+       } catch (Exception e) {
+            mv.addObject("msgErro", e.getMessage());
+            System.out.println("Erro ao salvar " + e.getMessage());
+            return mv;
+       }
+
+    }
+
+    @GetMapping("/inicio")
+    public ModelAndView home(){
+        ModelAndView mv =  new ModelAndView("home/index");
         return mv;
     }
     
