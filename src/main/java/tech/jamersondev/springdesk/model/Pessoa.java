fec1461c46d1f7cfadadbf30c554dbcacd5c4afa
Jamerson Souza <jamersonsouza@outlook.com>
2022-10-29 11:51:18 -0300

    Imports remocao

--- src/main/java/tech/jamersondev/springdesk/model/Pessoa.java
+++ src/main/java/tech/jamersondev/springdesk/model/Pessoa.java
@@ -1,6 +1,5 @@
 package tech.jamersondev.springdesk.model;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
