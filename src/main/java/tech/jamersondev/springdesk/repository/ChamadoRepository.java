3dc68998e7faa5ad030693f2c91c05d130b8b013
jamersonsouza <jamersonsouza@outlook.com>
2023-01-14 11:03:35 -0300

    Método para listar todos os chamados

--- src/main/java/tech/jamersondev/springdesk/repository/ChamadoRepository.java
+++ src/main/java/tech/jamersondev/springdesk/repository/ChamadoRepository.java
@@ -1,11 +1,17 @@
 package tech.jamersondev.springdesk.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import tech.jamersondev.springdesk.model.Chamado;
 
 @Repository
 public interface ChamadoRepository extends JpaRepository<Chamado, Integer>{
+
+    @Query(value = "select * from chamado", nativeQuery = true)
+    public List<Chamado> findAllChamados();
     
 }
