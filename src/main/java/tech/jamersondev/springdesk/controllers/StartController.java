da25cf69e09f6bf4ae742fdb51f772e339cf7d70
jamersonsouza <jamersonsouza@outlook.com>
2023-01-14 11:05:01 -0300

    Metodo de listagem dos chamados implementado

--- src/main/java/tech/jamersondev/springdesk/controllers/StartController.java
+++ src/main/java/tech/jamersondev/springdesk/controllers/StartController.java
@@ -1,16 +1,24 @@
 package tech.jamersondev.springdesk.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import tech.jamersondev.springdesk.repository.ChamadoRepository;
 
 
 @Controller
 public class StartController {
 
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
     @GetMapping("home")
     public ModelAndView start(){
         ModelAndView mv = new ModelAndView("home/index");
+        mv.addObject("chamadosList", chamadoRepository.findAllChamados());
         return mv;
     }
 
