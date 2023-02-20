922f189338a00c7e63a3ed9b59b4019ced5ad570
jamersonsouza <jamersonsouza@outlook.com>
2023-02-20 14:09:29 -0300

    implementacao do tecnicoUserDetails

---
+++ src/main/java/tech/jamersondev/springdesk/model/TecnicoUserDetailsImpl.java
@@ -0,0 +1,52 @@
+package tech.jamersondev.springdesk.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class TecnicoUserDetailsImpl implements UserDetails{
+
+    private Tecnico tecnico;
+
+    public TecnicoUserDetailsImpl(Tecnico tecnico){
+        this.tecnico = tecnico;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return tecnico.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return tecnico.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
