21c1065f52cbb46fbf11a712662df13c5cb49789
jamersonsouza <jamersonsouza@outlook.com>
2023-02-20 14:46:48 -0300

    metodo para buscar emails no bd

--- src/main/java/tech/jamersondev/springdesk/repository/TecnicoRepository.java
+++ src/main/java/tech/jamersondev/springdesk/repository/TecnicoRepository.java
@@ -1,5 +1,7 @@
 package tech.jamersondev.springdesk.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import tech.jamersondev.springdesk.model.Tecnico;
 
 @Repository
 public interface TecnicoRepository extends JpaRepository<Tecnico, Integer>{
-    
+
+    Optional<Tecnico> findByEmail(String email);
+
 }
