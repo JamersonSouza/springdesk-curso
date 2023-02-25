665ba97a026c5cdab04f518c66d48c0b5b63fc1e
jamersonsouza <jamersonsouza@outlook.com>
2023-02-25 10:52:38 -0300

    Construtores adicionado

--- src/main/java/tech/jamersondev/springdesk/model/Tecnico.java
+++ src/main/java/tech/jamersondev/springdesk/model/Tecnico.java
@@ -7,9 +7,22 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import tech.jamersondev.springdesk.Enums.Perfil;
+
+
+
 @Entity
 public class Tecnico extends Pessoa{
 
+    
+    
+    public Tecnico(Integer id, String nome, String email, String senha, String imagem, Perfil perfil) {
+        super(id, nome, email, senha, imagem, perfil);
+    }
+
+    public Tecnico(){
+
+    }
 
     @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
