f663e8920dcbe01edfc545a06ea3547771035c8b
jamersonsouza <jamersonsouza@outlook.com>
2023-03-15 21:50:51 -0300

    Implementacao do ClienteUserDetails

---
+++ src/main/java/tech/jamersondev/springdesk/model/ClienteUserDetailsImpl.java
@@ -0,0 +1,51 @@
+package tech.jamersondev.springdesk.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class ClienteUserDetailsImpl implements UserDetails{
+
+    private Cliente cliente;
+
+    public ClienteUserDetailsImpl(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return cliente.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+         return cliente.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+      return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
