46383b0688234f6e3d6b96d9aafa970a66e8590c
jamersonsouza <jamersonsouza@outlook.com>
2023-03-15 21:50:19 -0300

    Injecao de cliente

--- src/main/java/tech/jamersondev/springdesk/services/InjectDB.java
+++ src/main/java/tech/jamersondev/springdesk/services/InjectDB.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import tech.jamersondev.springdesk.Enums.Perfil;
+import tech.jamersondev.springdesk.model.Cliente;
 import tech.jamersondev.springdesk.model.Tecnico;
+import tech.jamersondev.springdesk.repository.ClienteRepository;
 import tech.jamersondev.springdesk.repository.TecnicoRepository;
 import tech.jamersondev.springdesk.util.PasswordUtil;
 
@@ -17,11 +19,17 @@ public class InjectDB {
     @Autowired
     private TecnicoRepository tecnicoRepository;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     public void injectDB(){
         Tecnico tecnico1 = new Tecnico(null, "Jamerson", "jamerson@mail.com",
          PasswordUtil.encoder("123456789"), null, Perfil.TECNICO);
 
+        Cliente cliente1 = new Cliente(null, "Tereza", "tereza@mail.com", PasswordUtil.encoder("123456789"),null, 
+        Perfil.CLIENTE);
+
+        clienteRepository.saveAll(Arrays.asList(cliente1));
          tecnicoRepository.saveAll(Arrays.asList(tecnico1));
     }
     
