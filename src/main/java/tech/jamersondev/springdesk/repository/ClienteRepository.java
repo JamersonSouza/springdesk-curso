0b7f830b5e52a3725f073997e646e76386b7cbb7
jamersonsouza <jamersonsouza@outlook.com>
2023-03-15 21:52:06 -0300

    Método para buscar email do cliente

--- src/main/java/tech/jamersondev/springdesk/repository/ClienteRepository.java
+++ src/main/java/tech/jamersondev/springdesk/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package tech.jamersondev.springdesk.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import tech.jamersondev.springdesk.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    Optional<Cliente> findByEmail(String email);
     
 }
