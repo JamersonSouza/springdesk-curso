15eb3fa93ffa94cef93df5f46977060f22db2389
jamersonsouza <jamersonsouza@outlook.com>
2023-03-15 21:51:43 -0300

    Construtores da classe cliente

--- src/main/java/tech/jamersondev/springdesk/model/Cliente.java
+++ src/main/java/tech/jamersondev/springdesk/model/Cliente.java
@@ -6,9 +6,20 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import tech.jamersondev.springdesk.Enums.Perfil;
+
 @Entity
 public class Cliente extends Pessoa{
+
     
+    
+    public Cliente(Integer id, String nome, String email, String senha, String imagem, Perfil perfil) {
+        super(id, nome, email, senha, imagem, perfil);
+    }
+
+    public Cliente() {
+    }
+
     @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
