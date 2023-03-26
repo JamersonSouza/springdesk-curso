af4e9114c3d3fcc16899c1a44f781e2c0deb373d
jamersonsouza <jamersonsouza@outlook.com>
2023-03-26 13:36:03 -0300

    Logout usuário

--- src/main/java/tech/jamersondev/springdesk/config/WebConfigProject.java
+++ src/main/java/tech/jamersondev/springdesk/config/WebConfigProject.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import tech.jamersondev.springdesk.services.ClienteUserDetailsService;
 import tech.jamersondev.springdesk.services.TecnicoUserDetailsService;
@@ -36,6 +37,12 @@ public class WebConfigProject extends WebSecurityConfigurerAdapter{
         .loginPage("/login")
         .defaultSuccessUrl("/")
         .permitAll();
+
+        http.logout()
+        .logoutRequestMatcher(
+            new AntPathRequestMatcher("/logout", "GET")
+        )
+        .logoutSuccessUrl("/login");
         
     }
 
