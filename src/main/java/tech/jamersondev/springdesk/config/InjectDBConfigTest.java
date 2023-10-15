6e50a70c9f12f5433c810cf5969be4e4f0d36875
jamersonsouza <jamersonsouza@outlook.com>
2023-10-14 23:00:13 -0300

    injeção de dados para perfil dev e test

--- src/main/java/tech/jamersondev/springdesk/config/InjectDBConfigTest.java
+++ src/main/java/tech/jamersondev/springdesk/config/InjectDBConfigTest.java
@@ -1,7 +1,6 @@
 package tech.jamersondev.springdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,23 +8,17 @@ import org.springframework.context.annotation.Profile;
 import tech.jamersondev.springdesk.services.InjectDB;
 
 @Configuration
-@Profile("dev")
-public class InjectDBConfig {
+@Profile("test")
+public class InjectDBConfigTest {
 
-    @Autowired
+     @Autowired
     private InjectDB injectDB;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String value;
-
     @Bean
-    public boolean injectDbinfos(){
-        if(value.equals("create")){
-            this.injectDB.injectDB();
-        }
-        return false;
+    public void injectDbinfos(){
+        this.injectDB.injectDBTest();
     }
-    
-    
+
+
     
 }
